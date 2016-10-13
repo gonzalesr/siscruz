4d13498c30538a429f450ba04bbea88351eb390f
Rolando Gonzales <rolandogonzales@outlook.com>
2016-10-13 09:59:03 -0400

    Agregando unidad de formulario

--- models/mrp_raw_material.py
+++ models/mrp_raw_material.py
@@ -12,17 +12,19 @@ class RawMaterial(models.Model):
     _description= 'Dato y caracterisiticas técnicas para producción - Materia Prima'
 
     ord_id = fields.Many2one('mrp.production', 'Orden de Produccion',required=1, ondelete='cascade')
-    name= fields.Selection([('bond', 'Bond'), ('termico', 'Térmico'), ('q-cb', 'Q - CB'),('q-cf','Q-CF'),
-        ('q-cfb', 'Q-CFB')],
+    name= fields.Selection([('Bond', 'Bond'), ('Termico', 'Térmico'), ('Q-CB', 'Q - CB'),('Q-CF','Q-CF'),
+        ('Q-CFB', 'Q-CFB')],
         string='Papel')
     kg = fields.Float(
         string='KG',digits=(6,2),required=False)
     paper_processing = fields.Float(
         string='Papel Procesado',digits=(6,2),required=False)
     total_load = fields.Float(
-        string='Carga Total',digits=(6,2),required=False)
+        string='Carga Total',digits=(8,2),required=False)
     coil_width = fields.Float(
         string='Ancho de Bobina',digits=(6,2),required=False)
     high_form = fields.Float(
         string='Alto de Formulario',digits=(6,2),required=False)
+    unid_high_form = fields.Char(
+        string='Unidad')
     armed = fields.Float(string='Armado',digits=(6,2),required=False)
